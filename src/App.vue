dcdda1177720b4313740b5fac8cf4ba19a84bb27
coding-to-music <connors.tom@gmail.com>
2020-11-14 04:19:08 -0500

    almost

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,75 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <header-bar />
+    <router-view
+      class="router-view" />
+    <div class="comments">
+      <div id="disqus_thread"></div>
+    </div>
+    <footer class="footer">
+      <div class="copyright">
+        <span class="copyright-icon">©</span>
+        michael-iriarte.com
+      </div>
+    </footer>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Header from './components/Header.vue'
 export default {
-  name: 'App',
+  name: 'app',
   components: {
-    HelloWorld
+    'header-bar': Header
+  },
+  mounted () {
+    if (process.env.NODE_ENV === 'production') {
+      const script = document.createElement('script')
+      script.src = 'https://mika-i.disqus.com/embed.js'
+      script.setAttribute('data-timestamp', +new Date())
+      document.body.appendChild(script)
+    }
   }
 }
 </script>
 
-<style>
+<style lang="scss">
+@import "styles/variables";
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  min-height: 100vh;
+
+  .router-view {
+    min-height: calc(100vh - 80px);
+    margin-top: 80px;
+  }
+  .comments {
+    margin: 20px;
+    #disqus_thread {
+      max-width: $nav-max-width;
+      margin: 0 auto;
+    }
+  }
+  .footer {
+    background: transparent;
+    text-align: center;
+    min-height: auto;
+    color: #030303;
+    font-size: 0.8rem;
+    vertical-align: middle;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    position: relative;
+    .copyright {
+      * {
+        color: inherit;
+        text-decoration: none;
+      }
+      .copyright-icon {
+        font-size: 0.6rem;
+      }
+    }
+  }
 }
 </style>
